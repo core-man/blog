687624b91983e2e6999a3eda2a1030d4d4d4a14a
core-man <coreman.seism@gmail.com>
2020-08-28 16:07:19 +0800

    update backup.pl

--- content/post/2020-03-15-backup/backup.pl
+++ content/post/2020-03-15-backup/backup.pl
@@ -30,10 +30,12 @@ foreach (@dir) {
     print STDERR "backup $_ at $now\n";
     print LOG "backup $_ at $now\n";
 
-    # rsync(远程同步)，更多参考https://www.howtoing.com/rsync-local-remote-file-synchronization-commands
-    # -a：归档模式。归档模式允许递归地拷贝文件，同时也保留了符号链接，文件权限，用户组所有权和时间戳。
-    # -v：详细
-    # --delete: 如果文件或目录在源目录中不存在，而在目标目录存在，同步时删除不存在源目录中的文件。
+    # rsync (远程同步):
+    #   https://www.howtoing.com/rsync-local-remote-file-synchronization-commands
+    #   https://segmentfault.com/a/1190000015669114
+    #   -a：归档模式。归档模式允许递归地拷贝文件，同时也保留了符号链接，文件权限，用户组所有权和时间戳。
+    #   -v：详细
+    #   --delete: 如果文件或目录在源目录中不存在，而在目标目录存在，同步时删除不存在源目录中的文件。
     system "rsync -av --delete $_ $BACK";
 }
 
